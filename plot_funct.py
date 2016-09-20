2b7f95a44806b91b28bb57ab5e146443d4ae5ae2
Sophia <student@otp583738ots.ucsf.edu>
2016-09-20 16:13:56 -0700

    plot_funct.py

---
+++ plot_funct.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot(x, y, freq)
+
+
+# set up 2D grid with numpy
+
+x, y = np.meshgrid(x, y)
+
+#convert intensity to numpy array
+
+freq = np.array(freq)
+
+
+#plug data into colormesh
+
+plt.pcolormesh(x, y, freq)
+plt.colorbar()
+#plt determines colorbar itself but we can customize
+plt.show()
