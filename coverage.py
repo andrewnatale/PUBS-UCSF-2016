e2d87f7ee2ad0b0ac601fbc05b369b36362b1a78
Andrew Natale <anatale@otp589576ots.ucsf.edu>
2016-09-20 15:55:39 -0700

    frequency

--- coverage.py
+++ coverage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2
 import cPickle as pic
+from collections import Counter
 
 allele_dict = pic.load(open('../allele_dic_with_WT.pkl','rb'))
 translate = pic.load(open('../translate.pkl','rb'))
@@ -11,7 +12,6 @@ amino_to_number = pic.load(open('../aminotonumber.pkl','rb'))
 
 translated_dict = {}
 for barcode in allele_dict:
-    #print barcode
     position, codon = allele_dict[barcode]
     if codon != 'WT':
         codon = codon.replace('T', 'U')
@@ -20,13 +20,5 @@ for barcode in allele_dict:
         #print str(position) + ' ' + str(codon) + ' ' + str(aa)
         translated_dict[barcode] = (position, aa, code_val)
 
-#print translated_dict
-#for stuff in translated_dict.values():
-#    print stuff[0]
-for code in sorted(translated_dict.values()):
-    print code
-
-#
-#
-#
-#
+frequency_data = Counter(translated_dict.values())
+print frequency_data
