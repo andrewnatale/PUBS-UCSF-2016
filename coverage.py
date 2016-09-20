ca3208992118f453ea42971fc76bedd969226dad
Andrew Natale <anatale@otp589576ots.ucsf.edu>
2016-09-20 14:50:21 -0700

    first file

---
+++ coverage.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2
+import cPickle as pic
+
+allele_dict = pic.load(open('../allele_dic_with_WT.pkl','rb'))
+translate = pic.load(open('../translate.pkl','rb'))
+amino_to_number = pic.load(open('../aminotonumber.pkl','rb'))
+
+#print allele_dict
+#print translate
+#print amino_to_number
+
+translated_dict = {}
+for barcode in allele_dict:
+    print barcode
+    position, codon = allele_dict[barcode]
+    if codon != 'WT':
+        codon = codon.replace('T', 'U')
+        aa = translate[codon]
+        print str(position) + ' ' + str(codon) + ' ' + str(aa)
