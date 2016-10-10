a0b8b10095186649bff12f767a8bae876cbc4768
Andrew Natale <anatale@udp081501uds.ucsf.edu>
2016-10-09 18:14:22 -0700

    andrews updates

--- image_analysis.py
+++ image_analysis.py
@@ -4,8 +4,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from skimage import data, io, filters, img_as_float, exposure, transform
-from skimage.morphology import disk, watershed
+from skimage.morphology import disk, watershed, opening, dilation, erosion
 from skimage.segmentation import slic, join_segmentations
+from skimage.feature import canny
 from scipy import fftpack
 from scipy import ndimage as ndi
 from sys import argv
@@ -13,17 +14,27 @@ from sys import argv
 script, filename = argv
 image = io.imread(filename)
 image_flt = img_as_float(image)
-image_sobel = filters.sobel(image)
+
+#median_filtered = filters.median(image, disk(3))
+#edges = canny(median_filtered/1., sigma=6)
+
+#image_sobel = filters.sobel(image)
 
 markers = np.zeros_like(image)
-markers[image < 3500] = 1
-markers[image > 5300] = 2
-
+#markers[image < 3600] = 2
+#markers[image > 3600] = 1
+markers[image > 5200] = 1
+fill = opening(markers)
+fill = opening(fill)
+fill = opening(fill)
+fill = opening(fill)
+fill = opening(fill)
+fill = opening(fill)
 #print image_sobel.min()
 #print image_sobel.max()
 
-seg = watershed(image_sobel, markers)
+#seg = watershed(image_sobel, markers)
 #print image.median()
-plt.imshow(seg, cmap=plt.cm.gray)
+plt.imshow(fill, cmap=plt.cm.gray)
 #plt.imshow(image, cmap=plt.cm.gray)
 plt.show()
