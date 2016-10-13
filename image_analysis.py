56131baed86be09d20af7443a32d023e03effa7e
Andrew Natale <anatale@16JTBS1.ucsf.edu>
2016-10-13 09:08:29 -0700

    segmentation

--- image_analysis.py
+++ image_analysis.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python2
 
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
-from skimage import data, io, filters, img_as_float, exposure, transform, measure
+from skimage import data, io, filters, img_as_float, exposure, transform, measure, feature
 from skimage.morphology import disk, watershed, opening, dilation, erosion, closing, white_tophat, black_tophat
 #from skimage.segmentation import slic, join_segmentations
 #from skimage.feature import canny
@@ -14,25 +15,89 @@ from sys import argv
 # imput and various iterations of image
 script, filename = argv
 image = io.imread(filename)
-image_flt = img_as_float(image)
-image_invert = 65535 - image
 
-def segment_image(image):
-    #z = closing(image, selem=np.ones((300,300)))
-    gaussian_filtered = filters.gaussian(image, sigma=3)
-    thresh = filters.threshold_otsu(gaussian_filtered)
-    binary = np.zeros_like(gaussian_filtered)
-    binary[gaussian_filtered < thresh] = 1
-    filled = ndi.morphology.binary_fill_holes(binary)
-    label_objects = measure.label(filled)
-    #sizes = np.bincount(label_objects.ravel())
-    #mask_sizes = sizes > 100
-    #mask_sizes[0] = 0
-    #cleaned = mask_sizes[label_objects]
+# segment a bright field image
+def segment_bf(image):
+    # convert to float
+    image = img_as_float(image)
+    original = image
+
+    # enhance contrast
+    p2, p98 = np.percentile(image, (2, 98))
+    image = exposure.rescale_intensity(image, in_range=(p2, p98))
+
+    # subtract a broad gaussian from the background
+    image = image - filters.gaussian(image, sigma=20)
+    image = image + abs(np.min(image))
+
+    # smooth and set a threshold
+    smoothed = filters.gaussian(image, sigma=3)
+    thresh = filters.threshold_otsu(smoothed)
+
+    # use threshold to create a binary mask
+    binary = np.zeros_like(image)
+    binary[smoothed < thresh*1.1] = 1
+    # fill holes
+    binary = ndi.morphology.binary_fill_holes(binary)
+
+
+    label_objects = measure.label(binary)
+    print 
+    """
+    ratio_thresh_mult = 1.5
     for region in measure.regionprops(label_objects):
-        print region.area
-        print region.perimeter
-    return (image, binary, label_objects)
+        if region.perimeter > ratio_thresh_mult \
+          * math.sqrt(4*math.pi*region.area):
+            print region.label
+    """
+    #plt.scatter(area, perimeter)
+    #plt.show()
+    #plt.hist(ratio)
+    #plt.show()
+    sizes = np.bincount(label_objects.ravel())
+    mask_sizes = np.zeros_like(sizes, dtype=bool)
+    for index, elem in enumerate(sizes):
+        if elem > 100 and elem < 5000:
+            mask_sizes[index] = 1
+    mask_sizes[0] = 0
+    cleaned = mask_sizes[label_objects]
+
+    #print type(mask_sizes)
+    #print mask_sizes.shape
+    #print mask_sizes
+
+    #print mask_sizes.shape
+    #print label_objects.ravel()
+    """
+
+    """
+    return (original, cleaned)
+
+def normalize(img):
+	high = np.amax(img)
+	low = np.amin(img)
+	return (img - low) / (high - low)
+
+
+def find_cells(img):
+	strong_blur = filters.gaussian(img, 20)
+	no_back = img - strong_blur
+	no_back = normalize(no_back)
+	equalized_no_back = exposure.equalize_hist(no_back)
+	equalized_no_back = normalize(equalized_no_back)
+	edges_nb = feature.canny(equalized_no_back, sigma=5)
+	close_nb = ndi.binary_closing(edges_nb, structure=np.ones((3, 3)), iterations=1)
+	fill_close_nb = ndi.binary_fill_holes(close_nb)
+	open_fcnb = ndi.binary_opening(fill_close_nb, structure=np.ones((10, 10)))
+	open_bigger = ndi.morphology.binary_dilation(open_fcnb, iterations=5)
+	#border = morph.binary_dilation(open_fcnb) - open_fcnb
+	#dist = ndi.distance_transform_edt(open_fcnb)
+	#local_peaks = feature.peak_local_max(dist, min_distance=12, threshold_abs=4,
+	#										labels=open_fcnb, indices=False)
+	#markers = ndi.label(local_peaks)[0]
+	#labels = morph.watershed(-dist, markers, mask=open_bigger)
+	#find_boundaries = seg.find_boundaries(labels)
+	return equalized_no_back, edges_nb
 
 
 #mask = np.zeros_like(image)
@@ -57,5 +122,12 @@ def image_comparison(image1, image2):
     #a.set_title('After2')
     plt.show()
 
-h, i, j = segment_image(image_flt)
-image_comparison(i, j)
+# image should be floating point
+def image_to_hist(image):
+    plt.hist(image.ravel(), bins=256, range=(0.0, 1.0), fc='k', ec='k')
+    plt.show()
+
+h, i = segment_bf(image)
+#h, i = find_cells(image)
+#image_comparison(h, i)
+#image_to_hist(h)
