cb5410c9e155b709f848fe173c2edce0012445a4
Andrew Natale <anatale@CCB2016-3.local>
2016-10-22 21:01:59 -0700

    reorganized

--- generate_fitness_scores.py
+++ generate_fitness_scores.py
@@ -1,60 +1,38 @@
+#!/usr/bin/env python2
 from scipy import stats
 import numpy as np
 import math
 import pickle as pkl
 import sys
 from collections import Counter
+from matplotlib import pyplot as plt
+from copy import deepcopy
 
-# open up pickle file with data
+# data outputs:
+# - array of per aa/pos raw fitness scores for every experiment + masks
+# - counts, frequency, r-squared for every codon
+
+# open up pickle file with data dictionary in the form:
+# 'barcode sequence' : [array of counts per barcode in each sample]
 with open(sys.argv[1], 'rb') as data_file:
-    data = pkl.load(data_file)
+    data_set = pkl.load(data_file)
+
+# data and dictionaries needed to process sequencing counts
 
 # open pickle file containing map of barcodes to alleles
 with open('allele_dic_with_WT.pkl', 'r') as allele_map_file:
     allele_map = pkl.load(allele_map_file)
-
 # open pickle file with codon to amino acid translations
 with open('translate.pkl', 'r') as translation_file:
     translation = pkl.load(translation_file)
-
 # 76 aa long ubqitin sequence
 ub_seq = 'MQIFVKTLTGKTITLEVESSDTIDNVKSKIQDKEGIPPDQQRLIFAGKQLEDGRTLSDYNIQKESTLHLVLRLRGG*'
-
-# delete any barcodes that have no counts
-start_count = len(data.keys())
-for barcode in data.keys():
-    if 0.0 in data[barcode]:
-        del data[barcode]
-end_count = len(data.keys())
-
-#print start_count, end_count
-
-# convert counts to frequency of each barcode in each sample
-for n in range(0,9):
-    sample_sum = np.sum(np.array([data[i][n] for i in data]))
-    for barcode in data:
-        data[barcode][n] = data[barcode][n] / sample_sum
-
-# log2 of the frequency
-for barcode in data:
-    data[barcode] = np.log2(data[barcode])
-
-# x vals (# of generations) for slope calculations
+# x vals (# of generations) for slope calculations from our experiments
 time_points = [0.0, 1.74, 3.71, 0.0, 1.75, 3.37, 0., 2., 4.]
 
-# calculate slope for every barcode for each experiment
-barcode_slopes = dict(zip(data.keys(), np.zeros((len(data.keys()), 3))))
-for barcode in data:
-    for n in range(0,3):
-        yvals = data[barcode][(n*3):(n*3+3)]
-        xvals = time_points[(n*3):(n*3+3)]
-        line = stats.linregress(xvals, yvals)
-        barcode_slopes[barcode][n] = line[0]
-
 # build a dictionary to help convert barcodes to mutants
-codon_list = sorted(Counter(allele_map.values()).keys())
 codon_to_aa = {}
-for codon in codon_list:
+for codon in sorted(Counter(allele_map.values()).keys()):
     if codon[1] == 'WT':
         if ub_seq[codon[0]-1] == '*':
             codon_to_aa[codon] = (codon[0], 'STOP')
@@ -63,26 +41,67 @@ for codon in codon_list:
     else:
         codon_to_aa[codon] = (codon[0], translation[codon[1].replace('T', 'U')])
 
-mutant_list = sorted(Counter(codon_to_aa.values()).keys())
-mutant_slopes = dict(zip(mutant_list, np.zeros((len(mutant_list), 4))))
+# aggregate counts to the codon level
+codon_list = sorted(Counter(allele_map.values()).keys())
+codon_counts = dict(zip(codon_list, np.zeros((len(codon_list), 9))))
+
+for barcode in data_set:
+    codon_counts[allele_map[barcode]] += data_set[barcode]
+
+# delete any codons that have no counts at any position
+start_count = len(codon_counts.keys())
+for codon in codon_counts.keys():
+    if 0.0 in codon_counts[codon]:
+        del codon_counts[codon]
+end_count = len(codon_counts.keys())
+
+# codon stats will hold (pos, codon):[]
+#codon_stats = dict(zip(codon_counts.keys(), np.zeros((len(codon_counts.keys()), 6))))
+
+# add log2 of raw codon counts to the stats dictionary
+#for codon in codon_counts:
+#        codon_stats[codon][0] = math.log(np.sum(codon_counts[codon][0:3]), 2)
+#        codon_stats[codon][2] = math.log(np.sum(codon_counts[codon][3:6]), 2)
+#        codon_stats[codon][4] = math.log(np.sum(codon_counts[codon][6:]), 2)
+
+# convert counts to frequency of each codon in each sample
+for n in range(0,9):
+    sample_sum = np.sum(np.array([codon_counts[i][n] for i in codon_counts]))
+    for codon in codon_counts:
+        codon_counts[codon][n] = codon_counts[codon][n] / sample_sum
+
+# log2 of the frequency
+for codon in codon_counts:
+     codon_counts[codon] = np.log2(codon_counts[codon])
+
+# calculate slope for every codon for each experiment
+codon_slopes = dict(zip(codon_counts.keys(), np.zeros((len(codon_counts.keys()), 6))))
+for codon in codon_counts:
+    for n in range(0,3):
+        yvals = codon_counts[codon][(n*3):(n*3+3)]
+        xvals = time_points[(n*3):(n*3+3)]
+        line = stats.linregress(xvals, yvals)
+        # store the slope and r-squared
+        codon_slopes[codon][n] = line[0]
+        codon_slopes[codon][n+3] = line[2]**2
+        #codon_stats[codon][n*2+1] = line[2]**2
 
 # get the average wt slope (only exact wt codons, not wt synonym)
 wt_slopes = np.zeros((1,3))
 wt_count = 0
-for barcode in barcode_slopes:
-    if allele_map[barcode][1] == 'WT':
-        wt_slopes += barcode_slopes[barcode][0:3]
+for codon in codon_slopes:
+    if codon[1] == 'WT':
+        wt_slopes += codon_slopes[codon][0:3]
         wt_count += 1
 wt_slopes = wt_slopes / float(wt_count)
 
-#print wt_count
-#print wt_slopes
-
-# sum up all the barcode slopes into mutant positions
-for barcode in barcode_slopes:
-    mutant_slopes[codon_to_aa[allele_map[barcode]]][0:3] \
-      += barcode_slopes[barcode][0:3]
-    mutant_slopes[codon_to_aa[allele_map[barcode]]][3] += 1
+# sum up all the codon slopes into mutant positions
+mutant_list = sorted(Counter(codon_to_aa.values()).keys())
+mutant_slopes = dict(zip(mutant_list, np.zeros((len(mutant_list), 4))))
+for codon in codon_slopes:
+    mutant_slopes[codon_to_aa[codon]][0:3] \
+      += codon_slopes[codon][0:3]
+    mutant_slopes[codon_to_aa[codon]][3] += 1
 
 # use the count to get the average
 for pos in sorted(mutant_slopes):
@@ -94,4 +113,35 @@ fitness_scores = {}
 for pos in mutant_slopes:
     fitness_scores[pos] = mutant_slopes[pos][0:3] - wt_slopes
 
-pkl.dump(fitness_scores, open("fitness_scores.pkl", 'wb'))
+#for codon in codon_stats:
+#    print codon, codon_stats[codon]
+
+def compare_hist(flat_array1, flat_array2):
+    fig = plt.figure()
+    a=fig.add_subplot(1,2,1)
+    hist = plt.hist(flat_array1)
+    a.set_title('barcode')
+    a=fig.add_subplot(1,2,2)
+    hist = plt.hist(flat_array2)
+    a.set_title('codon')
+    plt.show()
+
+def single_hist(flat_array):
+    plt.hist(flat_array, bins=500, range=(0.0, 1000.))
+    plt.show()
+
+#print np.sum(np.array(codon_counts.values()), axis=0)
+#print np.sum(np.array(codon_counts.values()))
+#print start_count, end_count
+
+pkl.dump(fitness_scores, open("fitness_scores1.pkl", 'wb'))
+
+#log_freqs = []
+#r_squareds = []
+#for codon in codon_stats:
+#    log_freqs.append(codon_stats[codon][0])
+#    log_freqs.append(codon_stats[codon][2])
+#    log_freqs.append(codon_stats[codon][4])
+#    r_squareds.append(codon_stats[codon][1])
+#    r_squareds.append(codon_stats[codon][3])
+#    r_squareds.append(codon_stats[codon][5])
--- process_fitness_data.py
+++ process_fitness_data.py
@@ -5,10 +5,18 @@ import matplotlib.pyplot as plt
 import pickle as pkl
 import sys
 from collections import Counter
+import pandas as pd
 #from plot_function import heatmap_function
 
-with open('fitness_scores.pkl', 'rb') as fitness_scores_file:
+with open('fitness_scores1.pkl', 'rb') as fitness_scores_file:
     fitness_scores = pkl.load(fitness_scores_file)
+
+ddg_data = pd.read_table('uby_1ubq.tsv')
+ddg_dict = {}
+for index, row in ddg_data.iterrows():
+    mutant = row['Mutation']
+    ddg_dict[(int(''.join(i for i in mutant[2:] if i.isdigit())), mutant[-1:])] = \
+      row['ddg_stability']
 
 # mapping of aa types to numbers that loosely
 # orders them from hydrophobic to polar with 0 as 'stop'
@@ -39,16 +47,31 @@ seq_length = len(Counter([i[0] for i in fitness_scores]))
 first_resi = int(sorted([i[0] for i in fitness_scores])[0])
 
 # create an empty 3d array of the correct size for all the data
-fitness_array = np.zeros((expreps+2,21,seq_length))
+fitness_array = np.zeros((expreps+3,21,seq_length))
 
 for elem in fitness_scores:
     coord = (int(aminotonumber[elem[1]]), int(elem[0]) - first_resi)
-    fitness_array[2:, coord[0], coord[1]] = fitness_scores[elem]
+    fitness_array[0:3, coord[0], coord[1]] = fitness_scores[elem]
     # this slice will be a mask that tells us which positions have no data
-    fitness_array[0,  coord[0], coord[1]] = 1
+    fitness_array[3, coord[0], coord[1]] = 1
     # this slice indicates wt or not
     if elem[1] == ub_seq[elem[0]-1]:
-        fitness_array[1,  coord[0], coord[1]] = 1
+        fitness_array[4,  coord[0], coord[1]] = 1
+    # this slice contains some rosetta ddg data
+    try:
+        ddg_score = ddg_dict[elem]
+    # if it can't be looked up, means its wt with score of 0
+    except KeyError:
+        ddg_score = 0.0
+    fitness_array[5, coord[0], coord[1]] = ddg_score
+
+# dictionary to help index the array
+data_sets = {'day1':0,
+             'day2':1,
+             'ctrl':2,
+             'data?':3,
+             'wt?':4,
+             'ddg':5}
 
 # compose data labels
 sequence_labels = []
@@ -63,7 +86,7 @@ for value in sorted(aminotonumber.values()):
             mutation_labels.append(key)
 #print mutation_labels
 
-def plot_hmap(data, row_labels, column_labels):
+def plot_hmap(data, row_labels, column_labels, _min=None, _max=None):
     sns.set(font_scale=1.2)
     grid_kws = {"height_ratios": (.9, .05), "hspace": .2}
     f, (ax, cbar_ax) = plt.subplots(2, gridspec_kw=grid_kws)
@@ -73,14 +96,16 @@ def plot_hmap(data, row_labels, column_labels):
                      cbar_ax=cbar_ax,
                      cbar_kws={"orientation": "horizontal"},
                      xticklabels=row_labels,
-                     yticklabels=column_labels)
+                     yticklabels=column_labels,
+                     vmin=_min,
+                     vmax=_max)
     plt.sca(ax)
     plt.yticks(rotation=0)
     plt.xticks(rotation=90)
     plt.show()
 
 # takes a 2D array
-def normalize_to_mean_stop(fitness_array):
+def threshold_to_mean_stop(fitness_array):
     # get max stop codon values for this replicate
     #max_stop = np.amax(fitness_array[aminotonumber['STOP'],:])
     avg_stop = np.mean(fitness_array[aminotonumber['STOP'],:])
@@ -91,13 +116,18 @@ def normalize_to_mean_stop(fitness_array):
     return fitness_array
 
 # takes a 2D array
-def normalize_to_max_stop(fitness_array):
+def threshold_to_max_stop(fitness_array):
     # get max stop codon values for this replicate
     max_stop = np.amax(fitness_array[aminotonumber['STOP'],:])
     #avg_stop = np.mean(fitness_array[aminotonumber['STOP'],:])
     # shift everything by the max stop codon value
     fitness_array = fitness_array - max_stop
     # neg values go to zero
+    fitness_array[fitness_array < 0] = 0
+    return fitness_array
+
+def arbitrary_threshold(fitness_array, threshold):
+    fitness_array = fitness_array - threshold
     fitness_array[fitness_array < 0] = 0
     return fitness_array
 
@@ -108,8 +138,20 @@ def normalize_info_content(fitness_array):
           / np.sum(fitness_array[:,i])
     return fitness_array
 
+# histogram of stop codon fitness scores, day1, day2, ctrl
+#plt.hist(fitness_array[[data_sets['day1'],data_sets['day2'],data_sets['ctrl']], aminotonumber['STOP'], :].flatten(), bins=20)
+#plt.show()
+
+# heatmap of ddg monomer data
+ddg_array = fitness_array[data_sets['ddg'],1:,:]
+print np.amin(ddg_array), np.amax(ddg_array)
+plt.hist(ddg_array.flatten(), bins=20)
+plt.show()
+#plot_hmap(ddg_array, sequence_labels, mutation_labels[1:], -10.0, 10.0)
+
 # average of two experiments
-#plot_hmap(np.mean(fitness_array[2:4,:,:], axis=0),sequence_labels,mutation_labels)
+#avg_perturb = np.mean(fitness_array[2:4,:,:], axis=0)
+#plot_hmap(arbitrary_threshold(avg_perturb, -0.2), sequence_labels, mutation_labels)
 
 # fitness difference between experimental and control conditions
 #plot_hmap(np.mean(fitness_array[2:4,:,:], axis=0) - fitness_array[4,:,:],sequence_labels,mutation_labels)
@@ -124,55 +166,10 @@ def normalize_info_content(fitness_array):
 #plot_hmap(fitness_array[1,:,:],sequence_labels,mutation_labels)
 
 # try normalization
-norm = normalize_info_content(normalize_to_max_stop(np.mean(fitness_array[2:4,:,:], axis=0)))
-plot_hmap(norm,sequence_labels,mutation_labels)
+#norm = normalize_info_content(normalize_to_mean_stop(np.mean(fitness_array[2:4,:,:], axis=0)))
+#plot_hmap(norm,sequence_labels,mutation_labels)
 
-
-"""
-# normalization types
-# None: don't do any normalization, results in
-# a mix of pos and neg values
-if normalization == None:
-    for index, j in np.ndenumerate(fitness_array):
-        if index in bad_data:
-            fitness_array[index] = 0.0
-    # average all the expreps
-    mean_fitness_array = np.mean(fitness_array, axis=0)
-# 'stop': use the highest stop codon fitness as a cutoff, set it to
-# zero and shift all the data, then discard values below zero
-elif normalization == 'stop':
-    for i in range(expreps):
-        # get max stop codon values for this replicate
-        max_stop = np.amax(fitness_array[i,aminotonumber['*'],:])
-        # shift everything by the max stop codon value
-        fitness_array[i,:,:] = fitness_array[i,:,:] - max_stop
-    # set bad data points and values below zero to zero
-    for index, j in np.ndenumerate(fitness_array):
-        if j < 0:
-            fitness_array[index] = 0.0
-        if index in bad_data:
-            fitness_array[index] = 0.0
-    # average all the expreps
-    mean_fitness_array = np.mean(fitness_array, axis=0)
-# 'prob': same as stop, but then normalize each position column
-# so that it sums to 1
-elif normalization == 'prob':
-    for i in range(expreps):
-        # get max stop codon values for this replicate
-        max_stop = np.amax(fitness_array[i,aminotonumber['*'],:])
-        # shift everything by the max stop codon value
-        fitness_array[i,:,:] = fitness_array[i,:,:] - max_stop
-    # set any values below zero to zero
-    for index, j in np.ndenumerate(fitness_array):
-        if j < 0:
-            fitness_array[index] = 0.0
-        if index in bad_data:
-            fitness_array[index] = 0.0
-    # average all the expreps
-    mean_fitness_array = np.mean(fitness_array, axis=0)
-    # normalize each position independently
-    for i in range(seq_length):
-        mean_fitness_array[:,i] = mean_fitness_array[:,i] \
-          / np.sum(mean_fitness_array[:,i])
-# end of the normalization loop
-"""
+#for index, val in np.ndenumerate(fitness_array[1,:,:]):
+#    if val == 1.:
+#        if fitness_array[0,index[0],index[1]] == 1.:
+#            print fitness_array[4,index[0],index[1]]
